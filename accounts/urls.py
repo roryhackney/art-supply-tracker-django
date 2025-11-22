73905fb5cc30e0ccde9f6ac5314fa6d7faf75f9e
roryhackney <roryhackney@gmail.com>
2025-11-22 13:34:58 -0800

    Static file setup, login/sigup updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import SignUpView
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name="signup")
+    path('signup/', SignUpView.as_view(), name="signup"),
+    path('login/', LoginView.as_view(redirect_authenticated_user=True), name='login'),
 ]
